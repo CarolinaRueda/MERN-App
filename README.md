a90a36a2904ea39a808d4ecee6b96875992caa65
CarolinaRueda <dianarueda99@gmail.com>
2023-01-24 18:17:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Welcome to my own project: MERN APP
+
+In this application, you can **register**:
+![Register preview](./1.jpg "Register")
+
+**Login**:
+![Login preview](./2.jpg "Login")
+
+And add all the **goals** you have:
+![Goals preview](./3.jpg "Add Goals")
+
+---
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+const protect = require("../middleware/authMiddleware");
+
+router.use(protect);
+
+router.route("/").get(getGoals).post(setGoal);
+
+router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
